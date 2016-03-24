922ce80c68572757e0aa0a70115923b398630749
Gabriel Duarte <gabrielqduarte@gmail.com>
2016-03-24 16:51:09 -0300

    Arrumado o título do site. Criado teste para o modelo Bundle.

---
+++ StardewValleyDatabase.Tests/Models/BundleTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StardewValleyDatabase.Models;
+using System;
+using System.Collections.Generic;
+
+namespace StardewValleyDatabase.Tests.Models
+{
+    [TestClass]
+    public class BundleTest
+    {
+        [TestMethod]
+        public void CriarBundle()
+        {
+            var nCdBundle = 1;
+            var sLinha = "Linha";
+            var sLocal = "Local";
+            var sNome = "Nome";
+            var sRecompensa = "Recompensa";
+            var lstItens = new List<BundleItem>()
+            {
+                new BundleItem()
+            };
+
+            Bundle oBundle = new Bundle()
+            {
+                nCdBundle = nCdBundle,
+                sLinha = sLinha,
+                sLocal = sLocal,
+                sNome = sNome,
+                sRecompensa = sRecompensa,
+                lstItens = lstItens
+            };
+
+            Assert.AreEqual(nCdBundle, oBundle.nCdBundle);
+            Assert.AreEqual(sLinha, oBundle.sLinha);
+            Assert.AreEqual(sLocal, oBundle.sLocal);
+            Assert.AreEqual(sNome, oBundle.sNome);
+            Assert.AreEqual(sRecompensa, oBundle.sRecompensa);
+            Assert.AreEqual(lstItens, oBundle.lstItens);
+        }
+    }
+}
